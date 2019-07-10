f2465d09c5507f3331930d73339a902b7940bda0
Mike VanSleen <mike.vansleen@gmail.com>
2019-07-10 14:53:48 -0600

    Getting NASA APOD data coming in

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
 import "./App.css";
 
 function App() {
+  const [imageOfTheDayURL, setImageOfTheDayURL] = useState("");
+
+  useEffect(() => {
+    axios
+      .get(
+        "https://api.nasa.gov/planetary/apod?api_key=Cvb4YpR38HIb85rJofCrCZuvofN5coBIpdSBaFOe"
+      )
+      .then(res => {
+        console.log(res.data.url);
+        setImageOfTheDayURL(res.data.url);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }, []);
+
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
-      </p>
+      <p>{imageOfTheDayURL}</p>
     </div>
   );
 }
