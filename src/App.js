d93207ddd41878ce2d76106160c869a772d7c4c1
Mike VanSleen <mike.vansleen@gmail.com>
2019-07-11 09:31:14 -0600

    Made specific components, styled with MUI

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React, { useState, useEffect } from "react";
 import NavBar from "./components/NavBar";
 import PhotoOfTheDay from "./components/PhotoOfTheDay";
+import PhotoDescription from "./components/PhotoDescription";
+import PhotoTitle from "./components/PhotoTitle";
 import axios from "axios";
 
 import "./App.css";
 
 function App() {
   const [imageOfTheDayURL, setImageOfTheDayURL] = useState("");
-  // const [] = useState;
+  const [imageOfTheDayDescription, setImageOfTheDayDescription] = useState("");
+  const [imageOfTheDayTitle, setImageOfTheDayTitle] = useState("");
 
   useEffect(() => {
     axios
@@ -15,8 +18,10 @@ function App() {
         "https://api.nasa.gov/planetary/apod?api_key=Cvb4YpR38HIb85rJofCrCZuvofN5coBIpdSBaFOe"
       )
       .then(res => {
-        console.log(res.data.url);
+        console.log(res.data);
         setImageOfTheDayURL(res.data.url);
+        setImageOfTheDayDescription(res.data.explanation);
+        setImageOfTheDayTitle(res.data.title);
       })
       .catch(err => {
         console.log(err);
@@ -26,6 +31,8 @@ function App() {
   return (
     <div className="App">
       <NavBar />
+      <PhotoTitle title={imageOfTheDayTitle} />
+      <PhotoDescription description={imageOfTheDayDescription} />
       <PhotoOfTheDay image={imageOfTheDayURL} />
     </div>
   );
